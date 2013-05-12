94d31ba265fa867b85766835130796be2dc74c55
Xyko <francisco@corp.globo.com>
2013-05-12 09:36:43 -0300

    teste

--- puppet/modules/xyko/manifests/server.pp
+++ puppet/modules/xyko/manifests/server.pp
@@ -25,7 +25,7 @@ class xyko::server inherits xyko::common {
         content => template("xyko/teste.erb"),
     }
 
-    #tools::rpm::release { ant: version => 'latest' }
+    tools::rpm::release { ant: version => 'latest' }
 
     tools::rpm::local { ant: version => 'latest' }
 
