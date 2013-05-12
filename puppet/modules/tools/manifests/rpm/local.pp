c257fbf2932c072bd14d335fb820709f3654ad00
Xyko <francisco@corp.globo.com>
2013-05-12 09:47:06 -0300

    teste

--- puppet/modules/tools/manifests/rpm/local.pp
+++ puppet/modules/tools/manifests/rpm/local.pp
@@ -1,4 +1,4 @@
-define tools::rpm::local( $package ) {
+define tools::rpm::local($package) {
     package { $package:
     source => "puppet:///tools/rpm/solr_generic_globo-4.2.1-2.el5.noarch.rpm"
    } 
