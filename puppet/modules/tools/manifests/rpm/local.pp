14c46c3980b26a25100536ebd927e1ca672986e6
Xyko <francisco@corp.globo.com>
2013-05-13 11:19:19 -0300

    teste

--- puppet/modules/tools/manifests/rpm/local.pp
+++ puppet/modules/tools/manifests/rpm/local.pp
@@ -7,7 +7,9 @@ define tools::rpm::local( $pack_name ) {
 
 		ensure   => installed,
 		provider => rpm,
-		source   => "/home/xyko/puppetTools/puppet/modules/tools/files/rpm//${pack_name}",
+		#source   => "/home/xyko/puppetTools/puppet/modules/tools/files/rpm//${pack_name}",
+		source   => "puppet:///puppet/modules/tools/files/rpm//${pack_name}",
+
 
 	} 
 
