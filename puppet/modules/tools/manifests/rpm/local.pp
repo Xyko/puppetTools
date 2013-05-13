760b56a12d4de5c8ea9a96676a3b837561457f91
Xyko <francisco@corp.globo.com>
2013-05-13 14:59:57 -0300

    teste

--- puppet/modules/tools/manifests/rpm/local.pp
+++ puppet/modules/tools/manifests/rpm/local.pp
@@ -3,13 +3,21 @@ define tools::rpm::local( $pack_name ) {
 	notice ($pack_name)
 	notice ($uptime)
 
-	package { $pack_name:
 
-		ensure   => installed,
-		provider => rpm,
-		#source   => "/home/xyko/puppetTools/puppet/modules/tools/files/rpm/${pack_name}",
-		source   => "puppet:///tools/rpm//${pack_name}",
+	file {
+	"/home/xyko/${pack_name}.txt":
+		ensure => file,
+		source => "puppet:///modules/tools/rpm/${pack_name}";
+	}
 
-	} 
+
+	# package { $pack_name:
+
+	# 	ensure   => installed,
+	# 	provider => rpm,
+	# 	#source   => "/home/xyko/puppetTools/puppet/modules/tools/files/rpm/${pack_name}",
+	# 	source   => "puppet:///tools/rpm//${pack_name}",
+
+	# } 
 
 }
