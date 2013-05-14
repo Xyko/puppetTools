ed1511197e0adc66c4cd34d2b5ce78355950598c
Xyko <francisco@corp.globo.com>
2013-05-14 14:49:12 -0300

    teste.

--- modules/xyko/manifests/server.pp
+++ modules/xyko/manifests/server.pp
@@ -45,14 +45,7 @@ class xyko::server inherits xyko::common {
         recurse => true,
     }
 
-    rvm_system_ruby {
-      'ruby-1.9.2-p290':
-        ensure => 'present',
-        default_use => true;
-      'ruby-1.8.7-p357':
-        ensure => 'present',
-        default_use => false;
-    }
+
 
 
 }
