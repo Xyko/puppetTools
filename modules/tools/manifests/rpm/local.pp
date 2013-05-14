2a72f57b2dfd00f0c044cb7d6471667d947d4940
Xyko <francisco@corp.globo.com>
2013-05-14 09:45:32 -0300

    teste.

--- modules/tools/manifests/rpm/local.pp
+++ modules/tools/manifests/rpm/local.pp
@@ -12,7 +12,7 @@ define tools::rpm::local( $pack_name ) {
 	package { $pack_name:
 		ensure   => present,
 		provider => rpm,
-		source	 => "puppet:///modules/tools/rpm/${pack_name}",
+		source	 => "puppet://modules/tools/rpm/${pack_name}",
 		#source   => "/tmp/${pack_name}",
 		#require => File["/tmp/${pack_name}"],
 	} 
