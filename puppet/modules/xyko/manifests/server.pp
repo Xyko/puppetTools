c30b49f63c683f1b034c2b20bb1dea5aa1eedc86
Xyko <francisco@corp.globo.com>
2013-05-14 06:30:45 -0300

    teste

--- puppet/modules/xyko/manifests/server.pp
+++ puppet/modules/xyko/manifests/server.pp
@@ -2,7 +2,7 @@ class xyko::server inherits xyko::common {
 
     include stdlib
 
-    notice(is_ip_address('190.10.3a.3'))
+    info(is_ip_address('190.10.3a.3'))
 
 	file {
 	"/etc/${projeto}.txt":
