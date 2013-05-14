bea9086ec3b1c51ba6be23fd56c096dd8a6422ae
Xyko <francisco@corp.globo.com>
2013-05-14 13:33:41 -0300

    teste.

--- modules/tools/manifests/rpm/release.pp
+++ modules/tools/manifests/rpm/release.pp
@@ -5,12 +5,12 @@ define tools::rpm::release( $package=false, $version ) {
     }
     # Usando variaveis do facter
     if ( $operatingsystemrelease >= 4 ) and ( $operatingsystemrelease < 5 ) {
-        $release = "el4" 
-    } 
-    else { 
+        $release = "el4"
+    }
+    else {
         if ( $operatingsystemrelease >= 5 ) and ( $operatingsystemrelease < 6 ) {
             $release = "el5"
-        } 
+        }
         else {
           if ( $operatingsystemrelease >= 6 ) and ( $operatingsystemrelease < 7 ) {
               $release = "el6"
@@ -18,8 +18,8 @@ define tools::rpm::release( $package=false, $version ) {
             # Poderiamos ter o el7, mas ainda não...
             fail( "Sistema operacional não suportado" )
           }
-        } 
-    } 
+        }
+    }
 
     if($version=="latest"){
         ## Qdo eh latest nao precisa colocar a release
