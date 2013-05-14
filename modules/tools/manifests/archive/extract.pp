e855551cf495a335daa47a6dfd1f57aab37ce1c0
Xyko <francisco@corp.globo.com>
2013-05-14 11:41:36 -0300

    teste.

--- modules/tools/manifests/archive/extract.pp
+++ modules/tools/manifests/archive/extract.pp
@@ -9,39 +9,39 @@ define tools::archive::extract (
     source => "puppet:///modules/tools/${name}/${name}.${extension}",
   }
 
-  # $extract_dir  = "${target}/${name}"
-  # $src_target   = "/tmp/${name}.${extension}"
+  $extract_dir  = "${target}/${name}"
+  $src_target   = "/tmp/${name}.${extension}"
 
-  # case $ensure {
-  #   present: {
+  case $ensure {
+    present: {
 
-  #     $extract_zip = "unzip -o ${src_target} -d ${target}"
-  #     $extract_targz = "tar --no-same-owner --no-same-permissions -xzf ${src_target} -C ${target}"
-  #     $extract_tarbz2 = "tar --no-same-owner --no-same-permissions -xjf ${src_target} -C ${target}"
+      $extract_zip = "unzip -o ${src_target} -d ${target}"
+      $extract_targz = "tar --no-same-owner --no-same-permissions -xzf ${src_target} -C ${target}"
+      $extract_tarbz2 = "tar --no-same-owner --no-same-permissions -xjf ${src_target} -C ${target}"
 
-  #     exec {"$name unpack":
-  #       command => $extension ? {
-  #         'zip' => "mkdir -p ${target} && ${extract_zip}",
-  #         'tar.gz' => "mkdir -p ${target} && ${extract_targz}",
-  #         'tgz' => "mkdir -p ${target} && ${extract_targz}",
-  #         'tar.bz2' => "mkdir -p ${target} && ${extract_tarbz2}",
-  #         'tgz2' => "mkdir -p ${target} && ${extract_tarbz2}",
-  #         default => fail ( "Unknown extension value '${extension}'" ),
-  #       },
-  #       creates => $extract_dir,
-  #       timeout => $timeout
-  #     }
-  #   }
-  #   absent: {
-  #     file {$extract_dir:
-  #       ensure => absent,
-  #       recurse => true,
-  #       purge => true,
-  #       force => true,
-  #     }
-  #   }
-  #   default: { err ( "Unknown ensure value: '${ensure}'" ) }
-  # }
+      exec {"$name unpack":
+        command => $extension ? {
+          'zip' => "mkdir -p ${target} && ${extract_zip}",
+          'tar.gz' => "mkdir -p ${target} && ${extract_targz}",
+          'tgz' => "mkdir -p ${target} && ${extract_targz}",
+          'tar.bz2' => "mkdir -p ${target} && ${extract_tarbz2}",
+          'tgz2' => "mkdir -p ${target} && ${extract_tarbz2}",
+          default => fail ( "Unknown extension value '${extension}'" ),
+        },
+        creates => $extract_dir,
+        timeout => $timeout
+      }
+    }
+    absent: {
+      file {$extract_dir:
+        ensure => absent,
+        recurse => true,
+        purge => true,
+        force => true,
+      }
+    }
+    default: { err ( "Unknown ensure value: '${ensure}'" ) }
+  }
 
 }
 
