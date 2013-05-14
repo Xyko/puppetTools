9f62a17f4ecbf566880129e878236544a0e5be1b
Xyko <francisco@corp.globo.com>
2013-05-14 11:58:34 -0300

    teste.

--- modules/tools/manifests/archive/extract.pp
+++ modules/tools/manifests/archive/extract.pp
@@ -20,7 +20,7 @@ define tools::archive::extract (
       $extract_tarbz2 = "tar --no-same-owner --no-same-permissions -xjf ${src_target} -C ${target}"
 
       exec {"$name unpack":
-        path => "/bin:/sbin:/usr/bin:/usr/sbin"
+        path => "/bin:/sbin:/usr/bin:/usr/sbin",
         command => $extension ? {
           'zip' => "sudo mkdir -p ${target} && ${extract_zip}",
           'tar.gz' => "sudo mkdir -p ${target} && ${extract_targz}",
@@ -30,7 +30,7 @@ define tools::archive::extract (
           default => fail ( "Unknown extension value '${extension}'" ),
         },
         creates => $extract_dir,
-        timeout => $timeout
+        timeout => $timeout,
       }
     }
     absent: {
