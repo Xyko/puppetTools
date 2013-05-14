e05bc99372b2110585db1f3a4d804997e53dc308
Xyko <francisco@corp.globo.com>
2013-05-14 13:20:25 -0300

    Revert "teste."
    This reverts commit 4e74e71e98192b2bf92f388e5ba3a4ce4e832c6f.

--- modules/xyko/manifests/server.pp
+++ modules/xyko/manifests/server.pp
@@ -1,7 +1,6 @@
 class xyko::server inherits xyko::common {
 
     include stdlib
-    include rvm
 
     notice is_ip_address('190.10.3a.3')
 
@@ -42,12 +41,10 @@ class xyko::server inherits xyko::common {
     }
 
     file { "/opt/nginx-1.4.0":
-      ensure    => directory,
-      owner     => "flipper3",
-      group     => "flipper3",
-      recurse   => true,
+      ensure => directory,
+      owner => "flipper3",
+      group => "flipper3",
+      recurse=>true,
     }
-
-
 
 }
