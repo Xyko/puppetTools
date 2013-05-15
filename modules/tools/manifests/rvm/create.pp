cabb3e74445ef68dc904fee86164ae6f4046349a
Xyko <francisco@corp.globo.com>
2013-05-15 10:14:29 -0300

    teste.

--- modules/tools/manifests/rvm/create.pp
+++ modules/tools/manifests/rvm/create.pp
@@ -6,8 +6,7 @@ define tools::rvm::create (
 
 	exec {"$name":
 	path    => "/bin:/sbin:/usr/bin:/usr/sbin",
-	command => "bash -c 'cd /home/flipper3 
-				&& curl -#L https://get.rvm.io | bash -s stable --autolibs=3 --ruby'",
+	command => "bash -c 'cd /home/flipper3 && curl -#L https://get.rvm.io | bash -s stable --autolibs=3 --ruby'",
 	timeout => $timeout,
 	}
 
