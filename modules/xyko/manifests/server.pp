41cf05b98ffbd5fcb44f3f8a17c9108a7371524d
Xyko <francisco@corp.globo.com>
2013-05-15 09:56:43 -0300

    teste.

--- modules/xyko/manifests/server.pp
+++ modules/xyko/manifests/server.pp
@@ -2,29 +2,29 @@ class xyko::server inherits xyko::common {
 
     notice is_ip_address('190.10.3a.3')
 
-	file {
-	"/etc/${projeto}.txt":
-		ensure => file,
-		source => "puppet:///modules/xyko/teste.txt",
-	}
+	# file {
+	# "/etc/${projeto}.txt":
+	# 	ensure => file,
+	# 	source => "puppet:///modules/xyko/teste.txt",
+	# }
 
-	tools::users::create { "flipper3":
-        usuario     => "flipper3",
-        grupo       => "flipper3",
-        uid         => "13102",
-        gid         => "13102",
-        password    => "flipper3",
-        home_dir	=> "/home/flipper3",
-	}
+	# tools::users::create { "flipper3":
+ #        usuario     => "flipper3",
+ #        grupo       => "flipper3",
+ #        uid         => "13102",
+ #        gid         => "13102",
+ #        password    => "flipper3",
+ #        home_dir	=> "/home/flipper3",
+	# }
 
-	# Initscripts
-    file { "/etc/init.d/teste":
-        ensure  => present,
-        owner   => "root",
-        group   => "root",
-        mode    => "755",
-        content => template("xyko/teste.erb"),
-    }
+	# # Initscripts
+ #    file { "/etc/init.d/teste":
+ #        ensure  => present,
+ #        owner   => "root",
+ #        group   => "root",
+ #        mode    => "755",
+ #        content => template("xyko/teste.erb"),
+ #    }
 
     #tools::rpm::release { ant: version => 'latest' }
 
