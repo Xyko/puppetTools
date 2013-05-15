dd5b76b1f7bae374ad87ee782c82a83fc33fcfb6
Xyko <francisco@corp.globo.com>
2013-05-15 12:07:29 -0300

    teste.

--- modules/tools/manifests/rvm/rubyinstall.pp
+++ modules/tools/manifests/rvm/rubyinstall.pp
@@ -3,7 +3,7 @@ define tools::rvm::rubyinstall {
 
 	exec {"$name":
 		path    	=> "/bin:/sbin:/usr/bin:/usr/sbin",
-		command 	=> "bash -c 'rvm install $(name)'",
+		command 	=> "bash -c 'rvm install ${name}'",
 		timeout 	=> $timeout,
 		logoutput	=> true,
 		user 		=> root,
