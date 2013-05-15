e7dcca1f3b7d47075d2a4255509eb85c4836e2d5
Xyko <francisco@corp.globo.com>
2013-05-15 16:14:07 -0300

    teste.

---
+++ modules/tools/manifests/rvm.pp
@@ -0,0 +1,25 @@
+define tools::rvm (
+	$ruby, )	{
+
+	case $name {
+		'ruby install': 	{ $command = "install 	${ruby}" }
+		'ruby uninstall': 	{ $command = "uninstall ${ruby}" }
+		'ruby reinstall': 	{ $command = "reinstall ${ruby}" }
+		'ruby remove': 		{ $command = "remove 	${ruby}" }
+		'gemset create': 	{ $command = "rvm use ${ruby} && rvm gemset create ${gemset}" }
+		'gemset delete': 	{ $command = "rvm use ${ruby} && rvm --force gemset delete ${gemset}" }
+		'gem install': 		{ $command = "rvm use ${ruby} && gem install ${gem}" }
+		'rvm create': 		{ $command = "bash -c 'curl -#L https://get.rvm.io | bash -s stable --autolibs=3 --ruby'" }
+		'rvm delete': 		{ $command = "rvm implode" }		
+		default:  			{ $command = "" }
+	}
+
+	exec {"$name":
+		path    	=> "/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/rvm/bin",
+		command 	=> "bash -lc ${command}",
+		timeout 	=> 600,
+		logoutput	=> true,
+		user 		=> root,
+	}
+
+}
--- modules/xyko/manifests/server.pp
+++ modules/xyko/manifests/server.pp
@@ -45,10 +45,10 @@ class xyko::server inherits xyko::common {
     #     recurse => true,
     # }
 
-    tools::rvm::install { "rvm create": }
-    tools::rvm::ruby    { "ruby remove":  ruby => "ruby-1.8.7-p371" }
-    tools::rvm::ruby    { "ruby install": ruby => "ruby-1.9.3-p429" }
-    tools::rvm::gem  { "gemset delete":   ruby => "ruby-1.9.3-p429" , gemset => "teste"}
+    tools::rvm { "rvm create": }
+    tools::rvm { "ruby remove":     ruby => "ruby-1.8.7-p371" }
+    tools::rvm { "ruby install":    ruby => "ruby-1.9.3-p429" }
+    tools::rvm { "gemset delete":   ruby => "ruby-1.9.3-p429" , gemset => "teste"}
 
 
 }
