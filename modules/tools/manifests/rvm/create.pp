e7d82cca2dd023fc99c8ebbace03484fe0ae5ee9
Xyko <francisco@corp.globo.com>
2013-05-15 09:22:45 -0300

    teste.

--- modules/tools/manifests/rvm/create.pp
+++ modules/tools/manifests/rvm/create.pp
@@ -1,11 +1,10 @@
 define tools::rvm::create (
 	owner,
-	$timeout
 	$timeout=600,
 	) {
 	notice $owner
 
-	exec {"$name execute":
+	exec {"$name install":
 	path    => "/bin:/sbin:/usr/bin:/usr/sbin",
 	command => "sudo bash -c 'cd ~ && curl -#L https://get.rvm.io | bash -s stable --autolibs=3 --ruby'",
 	timeout => $timeout,
