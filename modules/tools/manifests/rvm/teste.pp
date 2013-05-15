a6f563ef311d27e1d98918bbeea49b944e1a091f
Xyko <francisco@corp.globo.com>
2013-05-15 15:50:05 -0300

    teste.

--- modules/tools/manifests/rvm/teste.pp
+++ modules/tools/manifests/rvm/teste.pp
@@ -3,7 +3,7 @@ define tools::rvm::teste (){
 
 	exec {"rvm ${name}":
 		path    	=> "/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/rvm/bin",
-		command 	=> "bash -c 'rvm use ruby-1.9.3-p429 && gem list'",
+		command 	=> "bash -c 'rvm use ruby-1.9.3-p429 && rvm list'",
 		timeout 	=> 600,
 		logoutput	=> true,
 		user 		=> root,
