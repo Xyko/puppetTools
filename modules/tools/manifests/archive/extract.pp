152f9de6878e276bbb0455745277ec1d1d58cb33
Xyko <francisco@corp.globo.com>
2013-05-15 08:05:09 -0300

    teste.

--- modules/tools/manifests/archive/extract.pp
+++ modules/tools/manifests/archive/extract.pp
@@ -37,7 +37,7 @@ define tools::archive::extract (
       notice $extract_dir
       exec {"$name execute":
         path    => "/bin:/sbin:/usr/bin:/usr/sbin",
-        command => "bash -c 'cd  ${extract_dir} ; ./configure --prefix=/opt/nginx'",#" sudo make; sudo make install;'",
+        command => "bash -c 'cd  ${extract_dir} && ./configure --prefix=/opt/nginx && make && sudo make install'",
         timeout => $timeout,
       }
 
