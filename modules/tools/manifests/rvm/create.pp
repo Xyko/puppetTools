aa54dfa3c3b25bf7ad102ffaf2187507db9364af
Xyko <francisco@corp.globo.com>
2013-05-15 12:13:46 -0300

    teste.

--- modules/tools/manifests/rvm/create.pp
+++ modules/tools/manifests/rvm/create.pp
@@ -2,8 +2,8 @@ define tools::rvm::create {
 
 	exec {"$name":
 		path    	=> "/bin:/sbin:/usr/bin:/usr/sbin",
-		command 	=> "bash -c 'curl -#L https://get.rvm.io | bash -s stable --autolibs=3 --ruby'",
-		timeout 	=> $timeout,
+		command 	=> "bash -c './etc/profile.d/rvm.sh && curl -#L https://get.rvm.io | bash -s stable --autolibs=3 --ruby'",
+		timeout 	=> 600,
 		logoutput	=> true,
 		user 		=> root,
 	}
--- modules/tools/manifests/rvm/rubyinstall.pp
+++ modules/tools/manifests/rvm/rubyinstall.pp
@@ -1,10 +1,9 @@
 define tools::rvm::rubyinstall {
 
-
 	exec {"$name":
 		path    	=> "/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/rvm",
 		command 	=> "rvm install ${name}",
-		timeout 	=> $timeout,
+		timeout 	=> 600,
 		logoutput	=> true,
 		user 		=> root,
 	}
