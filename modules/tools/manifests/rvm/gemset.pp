30fccf28feccab35c64af05d6a5de2ec57c34e12
Xyko <francisco@corp.globo.com>
2013-05-15 14:47:38 -0300

    teste.

--- modules/tools/manifests/rvm/gemset.pp
+++ modules/tools/manifests/rvm/gemset.pp
@@ -5,14 +5,14 @@ define tools::rvm::gemset (
 	notice $gemset
 
     case $name {
-		'gem create': 	{ $command = "use ${ruby}@${gemset} --create" }
+		'gem create': 	{ $command = "rvm use ${ruby}@${gemset} --create" }
 		'gem delete': 	{ $command = "rvm ${ruby} do gemset delete ${gemset}" }
 		 default:  		{ $command = "" }
     }
 
 	exec {"$name":
 		path    	=> "/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/rvm/bin",
-		command 	=> "rvm use ruby-2.0.0-p195 && rvm gemset create ${gemset}",
+		command 	=> "${command}",
 		timeout 	=> 600,
 		logoutput	=> true,
 		user 		=> root,
