130a6f7f629f1fef4f19e718266ffb541633e4e3
Xyko <francisco@corp.globo.com>
2013-05-15 12:26:11 -0300

    teste.

--- modules/tools/manifests/rvm/create.pp
+++ modules/tools/manifests/rvm/create.pp
@@ -2,7 +2,7 @@ define tools::rvm::create {
 
 	exec {"$name":
 		path    	=> "/bin:/sbin:/usr/bin:/usr/sbin",
-		command 	=> "bash -c './etc/profile.d/rvm.sh && curl -#L https://get.rvm.io | bash -s stable --autolibs=3 --ruby'",
+		command 	=> "bash -c 'curl -#L https://get.rvm.io | bash -s stable --autolibs=3 --ruby'",
 		timeout 	=> 600,
 		logoutput	=> true,
 		user 		=> root,
