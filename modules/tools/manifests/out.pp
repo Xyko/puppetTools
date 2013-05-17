bb4d0d4b26e5f9c20a4236954c8c4eec504a8546
Xyko <francisco@corp.globo.com>
2013-05-17 14:39:50 -0300

    teste.

--- modules/tools/manifests/out.pp
+++ modules/tools/manifests/out.pp
@@ -1,8 +1,8 @@
 define tools::out ($msg='') {
 
-	$red="\033[01;31m"
-	$redalarm="\033[05;31m"
-	$normal="\033[00;37m"
+	$red="\\033[01;31m"
+	$redalarm="\\033[05;31m"
+	$normal="\\033[00;37m"
 
 	notice "${red}${msg}${normal}"
 
