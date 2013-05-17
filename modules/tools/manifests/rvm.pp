3e270a452f5ed0c3590d1a6103b7a774eb854dac
Xyko <francisco@corp.globo.com>
2013-05-17 08:55:29 -0300

    teste.

--- modules/tools/manifests/rvm.pp
+++ modules/tools/manifests/rvm.pp
@@ -20,12 +20,12 @@ define tools::rvm (
 
 	notice $command
 
-	exec {"$name":
-		path    	=> "/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/rvm/bin",
-		command 	=> "bash -lc ${command}",
-		timeout 	=> 600,
-		logoutput	=> true,
-		user 		=> root,
-	}
+	# exec {"$name":
+	# 	path    	=> "/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/rvm/bin",
+	# 	command 	=> "bash -lc ${command}",
+	# 	timeout 	=> 600,
+	# 	logoutput	=> true,
+	# 	user 		=> root,
+	# }
 
 }
