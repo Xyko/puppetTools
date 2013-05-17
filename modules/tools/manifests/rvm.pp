a2927eb37799b69eda4fe5773918b06249e25fcb
Xyko <francisco@corp.globo.com>
2013-05-17 08:54:25 -0300

    teste.

--- modules/tools/manifests/rvm.pp
+++ modules/tools/manifests/rvm.pp
@@ -6,7 +6,7 @@ define tools::rvm (
 	notice $name
 
 	case $name {
-		'ruby install': 	{ $command = "rvm install 	${ruby}" }
+		'ruby install': 	{ $command = "sudo rvm install 	${ruby}" }
 		'ruby uninstall': 	{ $command = "rvm uninstall ${ruby}" }
 		'ruby reinstall': 	{ $command = "rvm reinstall ${ruby}" }
 		'ruby remove': 		{ $command = "rvm remove 	${ruby}" }
@@ -22,7 +22,7 @@ define tools::rvm (
 
 	exec {"$name":
 		path    	=> "/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/rvm/bin",
-		command 	=> "sudo bash -lc ${command}",
+		command 	=> "bash -lc ${command}",
 		timeout 	=> 600,
 		logoutput	=> true,
 		user 		=> root,
