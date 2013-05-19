3ef57b3da091bfc169b61c481f8a4c0ac34483aa
Xyko <francisco@corp.globo.com>
2013-05-19 07:26:14 -0300

    teste.

--- modules/xyko/manifests/server.pp
+++ modules/xyko/manifests/server.pp
@@ -35,28 +35,23 @@ class xyko::server inherits xyko::common {
  #        content => template("xyko/teste.erb"),
  #    }
 
-# tools::rpm::release { ant: version => 'latest' }
-# tools::rpm::release { ant: version => 'latest' }
-# tools::rpm::release { ant: version => 'latest' }
-# tools::rpm::release { ant: version => 'latest' }
-# tools::rpm::release { ant: version => 'latest' }
-# tools::rpm::release { ant: version => 'latest' }
-# tools::rpm::release { ant: version => 'latest' }
-# tools::rpm::release { ant: version => 'latest' }
-# tools::rpm::release { ant: version => 'latest' }
-# tools::rpm::release { ant: version => 'latest' }
-# tools::rpm::release { ant: version => 'latest' }
-# tools::rpm::release { ant: version => 'latest' }
-# tools::rpm::release { ant: version => 'latest' }
-# tools::rpm::release { ant: version => 'latest' }
+    # tools::users::create { "xyko":
+ #        usuario     => "xyko",
+ #        grupo       => "xyko",
+ #        uid         => "10000",
+ #        gid         => "10000",
+ #        password    => "fc4vbRND",
+ #        home_dir  => "/home/xyko",
+    # }
 
-    tools::rpm::yum {yum: packlist => ["gcc-c++","patch","readline","ImageMagick"]}
-    tools::rpm::yum {yum: packlist => ["readline-devel","zlib","zlib-devel","libyaml-devel"]}
-    tools::rpm::yum {yum: packlist => ["libffi-devel","openssl-devel","gcc","ruby-devel","libxml2","libxml2-devel","libxslt"]}
-    tools::rpm::yum {yum: packlist => ["libxslt-devel","curl","curl-devel","libevent","libevent-devel","intltool"]}
-    tools::rpm::yum {yum: packlist => ["mysql-server","sqlite-devel","mysql-devel","mysql-client","strace","ntp","puppet"]}
-    # tools::rpm::local { transmission-common:  options => "--nogpgcheck", pack_name => 'transmission-common-2.04-2.fc14.1.x86_64.rpm' }
-    # tools::rpm::local { transmission-gtk: options => "--nogpgcheck", pack_name => 'transmission-gtk-2.04-2.fc14.1.x86_64.rpm' }
+
+    tools::rpm::yum {time(): packlist => ["gcc-c++","patch","readline","ImageMagick"]}
+    tools::rpm::yum {time(): packlist => ["readline-devel","zlib","zlib-devel","libyaml-devel"]}
+    tools::rpm::yum {yum3: packlist => ["libffi-devel","openssl-devel","gcc","ruby-devel","libxml2","libxml2-devel","libxslt"]}
+    tools::rpm::yum {yum4: packlist => ["libxslt-devel","curl","curl-devel","libevent","libevent-devel","intltool"]}
+    tools::rpm::yum {yum5: packlist => ["mysql-server","sqlite-devel","mysql-devel","mysql-client","strace","ntp","puppet"]}
+    tools::rpm::local { transmission-common:  options => "--nogpgcheck", pack_name => 'transmission-common-2.04-2.fc14.1.x86_64.rpm' }
+    tools::rpm::local { transmission-gtk: options => "--nogpgcheck", pack_name => 'transmission-gtk-2.04-2.fc14.1.x86_64.rpm' }
 
 
 
