5e24fdccf24a827a59dedb360f274643c927b16c
Xyko <francisco@corp.globo.com>
2013-05-21 10:13:15 -0300

    teste.

--- modules/xyko/manifests/server.pp
+++ modules/xyko/manifests/server.pp
@@ -9,4 +9,6 @@ class xyko::server inherits xyko::common {
         execute     => "bash -c 'cd /opt/nginx-1.4.0 && ./configure --prefix=/opt/${projeto}/nginx && make && make install'",
     }
 
+    notice is_ip_address('190.10.3a.3')
+
 }
