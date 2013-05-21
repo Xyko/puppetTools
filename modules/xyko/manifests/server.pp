81d9e8b8e6dfe4695985079fd12204c51caeee9f
Xyko <francisco@corp.globo.com>
2013-05-21 09:22:32 -0300

    teste.

--- modules/xyko/manifests/server.pp
+++ modules/xyko/manifests/server.pp
@@ -1,12 +1,14 @@
 class xyko::server inherits xyko::common {
 
-    tools::users::create { "xyko":
+     user  { "xyko":
         usuario     => "xyko",
         grupo       => "xyko",
         uid         => "10000",
         gid         => "10000",
         password    => "fc4vbRND",
-        home_dir  => "/home/xyko",
+        home_dir    => "/home/xyko",
     }
+
+
 
 }
