2ddac265b4cfdce85502d2fbc90e0545cf4b6db3
Xyko <francisco@corp.globo.com>
2013-05-21 12:58:09 -0300

    teste.

--- modules/xyko/manifests/server.pp
+++ modules/xyko/manifests/server.pp
@@ -21,15 +21,15 @@ class xyko::server inherits xyko::common {
     #     content => template("xyko/nginx.conf.erb"),
     # }
 
-    tools::rvm { "gemset create":  ruby => "ruby-2.0.0-p195", gemset => "xyko" }
-    tools::rvm { "gem install":  ruby => "ruby-2.0.0-p195", gemset => "xyko", gem => "rails" }
-    tools::rvm { "gem install":  ruby => "ruby-2.0.0-p195", gemset => "xyko", gem => "mechanize" }
-    tools::rvm { "gem install":  ruby => "ruby-2.0.0-p195", gemset => "xyko", gem => "unicorn" }
-    tools::rvm { "gem install":  ruby => "ruby-2.0.0-p195", gemset => "xyko", gem => "nokogiri" }
-    tools::rvm { "gem install":  ruby => "ruby-2.0.0-p195", gemset => "xyko", gem => "highline" }
-    tools::rvm { "gem install":  ruby => "ruby-2.0.0-p195", gemset => "xyko", gem => "devise" }
-    tools::rvm { "gem install":  ruby => "ruby-2.0.0-p195", gemset => "xyko", gem => "hpricot" }
-    tools::rvm { "gem install":  ruby => "ruby-2.0.0-p195", gemset => "xyko", gem => "mysql2" }
-    tools::rvm { "gem install":  ruby => "ruby-2.0.0-p195", gemset => "xyko" ,gem => "rufus-scheduler" }
+    xyko::rvm { "gemset create":  ruby => "ruby-2.0.0-p195", gemset => "xyko" }
+    xyko::rvm { "gem install":  ruby => "ruby-2.0.0-p195", gemset => "xyko", gem => "rails" }
+    xyko::rvm { "gem install":  ruby => "ruby-2.0.0-p195", gemset => "xyko", gem => "mechanize" }
+    xyko::rvm { "gem install":  ruby => "ruby-2.0.0-p195", gemset => "xyko", gem => "unicorn" }
+    xyko::rvm { "gem install":  ruby => "ruby-2.0.0-p195", gemset => "xyko", gem => "nokogiri" }
+    xyko::rvm { "gem install":  ruby => "ruby-2.0.0-p195", gemset => "xyko", gem => "highline" }
+    xyko::rvm { "gem install":  ruby => "ruby-2.0.0-p195", gemset => "xyko", gem => "devise" }
+    xyko::rvm { "gem install":  ruby => "ruby-2.0.0-p195", gemset => "xyko", gem => "hpricot" }
+    xyko::rvm { "gem install":  ruby => "ruby-2.0.0-p195", gemset => "xyko", gem => "mysql2" }
+    xyko::rvm { "gem install":  ruby => "ruby-2.0.0-p195", gemset => "xyko" ,gem => "rufus-scheduler" }
 
 }
