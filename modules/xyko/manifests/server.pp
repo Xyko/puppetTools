c2c0c24fe7cacb1b8463a8d40b83ea2eb58e6827
Xyko <francisco@corp.globo.com>
2013-05-21 12:12:39 -0300

    teste.

--- modules/xyko/manifests/server.pp
+++ modules/xyko/manifests/server.pp
@@ -2,16 +2,23 @@ class xyko::server inherits xyko::common {
 
     include xyko::users
 
-    # xyko::extract {"nginx-1.4.0":
-    #     ensure      => present,
-    #     target      => "/opt/",
-    #     extension   => 'tar.gz',
-    #     execute     => "bash -c 'cd /opt/nginx-1.4.0 && ./configure --prefix=/opt/${projeto}/nginx && make && make install'",
-    # }
-
-    xyko::yumlocal { transmission-common:  options => "--nogpgcheck", pack_name => 'transmission-common-2.04-2.fc14.1.x86_64.rpm' }
-    xyko::yumlocal { transmission-gtk: options => "--nogpgcheck", pack_name => 'transmission-gtk-2.04-2.fc14.1.x86_64.rpm' }
-
     notice is_ip_address('190.10.3.3')
+    debug_msg("alert", "Send as a debug message")
+
+    xyko::extract {"nginx-1.4.0":
+        ensure      => present,
+        target      => "/opt/",
+        extension   => 'tar.gz',
+        execute     => "bash -c 'cd /opt/nginx-1.4.0 && ./configure --prefix=/opt/${projeto}/nginx && make && make install'",
+    }
+
+    # Initscripts
+    file { "/opt/${projeto}/nginx/conf/nginx.conf":
+        ensure  => present,
+        owner   => "root",
+        group   => "root",
+        mode    => "755",
+        content => template("xyko/nginx.erb"),
+    }
 
 }
